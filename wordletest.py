637097b0d5676c50dd8c9803f5cd2d0d2a343394
alexale1100 <ale10@tulane.edu>
2024-12-14 20:27:06 -0600

    wordletest.py

--- wordletest.py
+++ wordletest.py
@@ -1,28 +1,32 @@
-from wordlefinal_fortesting import *
-
-def test_invalid_guess():
-    output_of_game = wordle_game(["asdf", "spine"], "spine")
-    assert output_of_game[1] == "Invalid guess. Please enter a valid 5-letter word."
-
-from wordlefinal_fortesting import *
+from wordlesolver import *
 
 # test valid guess
 
 def test_invalid_guess():
-    output_of_game = wordle_game(["asdf", "spine"], "spine")
-    assert output_of_game[1] == "Invalid guess. Please enter a valid 5-letter word."
+    output_of_game = wordle_solver([”asde“, ”spine“], ”spine“)
+    # assert output_of_game[1] == ”Invalid guess. Please enter a valid 5-letter word.“
 
 # test winning the game
 
 def test_correct_guess():
-    target_word = "spine"
-    output_of_game = wordle_game(["glass", "glass", "ready", "ready", "spine"], target_word)
-    assert output_of_game[1] == f"Congratulations! You've guessed the word '{target_word}'!"
+    target_word = ”spine“
+    output_of_game = wordle_solver([”glass“, ”glass“, ”ready“, ”ready“, ”spine“], target_word)
+    # assert output_of_game[1] == f”Congratulations! You‘ve guessed the word ’{target_word}‘!“
 
 
 # test failing the game
 def test_fail_game():
-    words_to_guess = ["hello", "ready", "ready", "ready", "ready", "ready"]
-    target_word = "drive"
-    output_of_game = wordle_game(words_to_guess, target_word)
-    assert output_of_game[1] == "Game over! The word was '{target_word}'."
+    words_to_guess = [”hello“, ”ready“, ”ready“, ”ready“, ”ready“, ”ready“]
+    target_word = ”drive“
+    output_of_game = wordle_solver(words_to_guess, target_word)
+    # assert output_of_game[1] == ”Game over! The word was ’{target_word}‘.“
+
+
+print(”test invalid guess“)
+test_invalid_guess()
+
+print(”test correct guess“)
+test_correct_guess()
+
+print(”test fail game“)
+test_fail_game()
